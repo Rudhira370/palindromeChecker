ed2054ba60d67626a7bd1ae244a4ac3969f4cff7
Rudhira370 <rudhira370@gmail.com>
2026-02-24 15:28:41 +0530

    [Anshul] completed uc1

---
+++ app/src/palindromCheckerapp.java
@@ -0,0 +1,7 @@
+public class palindromCheckerapp {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome checker app");
+        System.out.println("version 2.0");
+        System.out.println("system initialized successfully");
+    }
+}
